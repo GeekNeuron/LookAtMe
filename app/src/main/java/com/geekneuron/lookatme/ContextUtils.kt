88838dfda5022a0bba5517e12fe73e3d63c10059
Geek Neuron <geekneuron@gmail.com>
2025-07-18 11:19:39 +0330

    Create ContextUtils.kt

---
+++ app/src/main/java/com/geekneuron/lookatme/ContextUtils.kt
@@ -0,0 +1,13 @@
+package com.yourcompany.lookatme
+
+import android.content.Context
+import java.util.Locale
+
+object ContextUtils {
+    fun updateLocale(context: Context, localeToSwitchTo: Locale): Context {
+        val resources = context.resources
+        val configuration = resources.configuration
+        configuration.setLocale(localeToSwitchTo)
+        return context.createConfigurationContext(configuration)
+    }
+}
